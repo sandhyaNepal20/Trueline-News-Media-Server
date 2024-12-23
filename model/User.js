52d9680da79b637b5a29f8dd293e52e4326a09bc
sandhyaNepal20 <nsandhya192@gmail.com>
2024-12-23 22:15:55 +0545

    Day1: Folders created for controllers,models and routes

---
+++ model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    full_name: {
+        type: String,
+        rewuired: true
+    },
+    email: {
+        type: String,
+        required: true
+
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("users", userSchema)
+module.exports = User;
