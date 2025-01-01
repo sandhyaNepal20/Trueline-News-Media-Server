0dcea8b9bfe8f034bd46c5ace6b8c3f75d86b3f2
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-01 22:02:00 +0545

    user registration api implemented

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const connectDb = require("./config/db")
+const UserRouter = require("./routes/UserRoute")
 
 const app = express();
 
 connectDb();
 
-
-
+app.use(express.json());
+app.use("/api/user", UserRouter);
 const port = 3000;
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`)
