7a7857db7bd34d24b579631ac23921aee352af19
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-04 21:56:24 +0545

    NewsRoute created

---
+++ routes/NewsRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { findAll, save, findbyId, deletebyId, update } = require("../controller/NewsController");
+const router = express.Router();
+
+router.get("/", findAll);
+router.post("/", save);
+router.get("/:id", findbyId)
+router.delete("/:id", deletebyId)
+router.put("/:id", update)
+
+
+module.exports = router;
