bf170f7576998e3bfa03c0a7613e36d0b14e5757
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-06 19:17:31 +0545

    Comment model created

---
+++ model/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const commentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    },
+    newsId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "news_article"
+    },
+
+    content: {
+        type: String,
+        required: true
+    },
+    comment_date: {
+        type: String,
+        required: true
+    }
+})
+
+const comment = mongoose.model("comments", commentSchema)
+module.exports = comment;
