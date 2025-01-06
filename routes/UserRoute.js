4978663ba7f65eadde6715b997413fd07966f802
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-06 19:43:06 +0545

    Changes in usercontroller file name

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { findAll, save, findbyId, deletebyId, update } = require("../controller/userController");
+const { findAll, save, findbyId, deletebyId, update } = require("../controller/UserController");
 const router = express.Router();
 
 router.get("/", findAll);
