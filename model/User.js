4c2d47eca419f96fab614486c28119fba11feb60
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-07 22:06:17 +0545

    Admin model created

--- model/User.js
+++ model/User.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         rewuired: true
     },
+    username: {
+        type: String,
+        rewuired: true
+    },
     email: {
         type: String,
         required: true
