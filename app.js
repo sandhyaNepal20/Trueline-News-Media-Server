802e243287464a2c2df774320f002e7302b19c8b
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-07 08:58:11 +0545

    RoutePath added in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const connectDb = require("./config/db")
 const UserRouter = require("./routes/UserRoute")
+const CategoryRouter = require("./routes/CategoryRoute")
+const NewsRouter = require("./routes/NewsRoute")
+const CommentRouter = require("./routes/CommentRoute")
 
 const app = express();
 
@@ -8,6 +11,11 @@ connectDb();
 
 app.use(express.json());
 app.use("/api/user", UserRouter);
+app.use("/api/news", NewsRouter);
+app.use("/api/category", CategoryRouter);
+app.use("/api/comment", CommentRouter);
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`)
