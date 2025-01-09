30af10ad1190409075e611e4ec811d6c26e55e4f
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-09 22:39:26 +0545

    Changes in Auth.js

--- security/Auth.js
+++ security/Auth.js
@@ -1,32 +1,28 @@
-const jwt = require("jsonwebtoken")
-
+const jwt = require("jsonwebtoken");
 const SECRET_KEY = "5b00dad07e7bb40f0b69fc27313440c985f8f63b645df1cda38036244a18216e";
-
 
 function authenticateToken(req, res, next) {
     const token = req.header("Authorization")?.split(" ")[1];
     if (!token) {
-        return res.status(401).send("Access denied: No token provided")
+        res.status(401).send("Access Denied: No token provided")
     }
+
     try {
-        const verified = jwt.varify(token, SECRET_KEY)
+        const verified = jwt.verify(token, SECRET_KEY)
         req.user = verified;
         next()
     } catch (e) {
         res.status(400).send("Invalid token")
     }
-
-
 }
 
 function authorizeRole(role) {
-
     return (req, res, next) => {
         if (req.user.role !== role) {
-            return res.status(403).send("Access Denied: Insufficient Permissions")
+            return res.status(403).send("Assess Denied: Insufficient Permission")
 
-        }
-        next();
+        } next();
     }
+
 }
 module.exports = { authenticateToken, authorizeRole }
