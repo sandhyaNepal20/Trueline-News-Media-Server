300f5f81a220eb0503337193ef74221462e89457
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-09 22:46:51 +0545

    authenticatetoken in authroute

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const { login, register } = require("../controller/AuthController");
-// const { authenticateToken } = require("../security/Auth");
+// const { authenticateToken, authorizeRole } = require("../security/Auth");
 
 
 router.post("/login", login);
